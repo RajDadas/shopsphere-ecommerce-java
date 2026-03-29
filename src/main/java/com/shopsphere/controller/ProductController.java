24b23c1b72c60d5b4581b4b869f44e23b8103d33
Rajdatta Kashinath dadas <101989702+RajDadas@users.noreply.github.com>
2026-03-29 18:15:03 +0530

    Refactor Product instantiation in add method

--- src/main/java/com/shopsphere/controller/ProductController.java
+++ src/main/java/com/shopsphere/controller/ProductController.java
@@ -1,6 +1,6 @@
-
 package com.shopsphere.controller;
 
+import com.shopsphere.entity.Product;
 import com.shopsphere.repo.ProductRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,7 +23,7 @@ public class ProductController {
 
     @PostMapping("/add")
     public String add(@RequestParam String name, @RequestParam double price) {
-        var p = new com.shopsphere.entity.Product();
+        Product p = new Product();
         p.setName(name);
         p.setPrice(price);
         repo.save(p);
