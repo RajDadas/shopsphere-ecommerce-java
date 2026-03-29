0d997ecd874e3e90a52efb8f754054d0202efd49
Rajdatta Kashinath dadas <101989702+RajDadas@users.noreply.github.com>
2026-03-29 10:11:23 +0530

    Delete java/com/shopsphere directory

--- java/com/shopsphere/repo/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-
-package com.shopsphere.repo;
-
-import com.shopsphere.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {}
