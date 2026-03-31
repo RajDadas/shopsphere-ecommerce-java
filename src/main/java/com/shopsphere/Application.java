b4cb2a34de11ecba121572e7ceac407eaf591520
Rajdatta Kashinath dadas <101989702+RajDadas@users.noreply.github.com>
2026-03-31 11:32:27 +0530

    Fixed authentication configuration and password encoding

--- src/main/java/com/shopsphere/Application.java
+++ src/main/java/com/shopsphere/Application.java
@@ -9,7 +9,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
