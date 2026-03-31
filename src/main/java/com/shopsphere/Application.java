2bd4b273c5ea178b9659785c313cda5820b1b04d
Rajdatta Kashinath dadas <101989702+RajDadas@users.noreply.github.com>
2026-03-31 10:44:33 +0530

    Added default admin user with encrypted password
    Add CommandLineRunner to initialize admin user if not present.

--- src/main/java/com/shopsphere/Application.java
+++ src/main/java/com/shopsphere/Application.java
@@ -3,10 +3,25 @@ package com.shopsphere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.shopsphere.entity.User;
+import com.shopsphere.repo.UserRepo;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    CommandLineRunner run(UserRepo repo, BCryptPasswordEncoder encoder) {
+        return args -> {
+            if (repo.findByUsername("admin").isEmpty()) {
+                User user = new User();
+                user.setUsername("admin");
+                user.setPassword(encoder.encode("admin123"));
+                repo.save(user);
+            }
+        };
     }
 }
